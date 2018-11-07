048ae58031dccc7996c52bb947a8a984f683ccb6
Fahad Kamran <fhdkmrn@gmail.com>
2018-11-07 17:06:57 -0500

    Add Timeseries Transform, Base LSTM Model

---
+++ lib/bball_transform/ts_transform.py
@@ -0,0 +1,72 @@
+''' 
+transform takes a legacy.possession as input and output data needed for __getitem__
+for PyTorch dataset. To add in order of players, refer to 
+https://gitlab.eecs.umich.edu/jiaxuan/bball2017/blob/master/lib/training/dataset.py
+'''
+from lib.process_possession import cutOnce
+import numpy as np
+import torch
+
+def order_team_players(players, gamecode):
+    # todo: sort by position
+    # see https://gitlab.eecs.umich.edu/jiaxuan/bball2017/blob/master/lib/training/dataset.py
+    return sorted(players, key=lambda p: p.id)
+                  
+def order_players(frame, offense_team):
+    players = frame.players
+    gamecode = frame.frameInfo.gamecode
+    # first order by teams, offense team first
+    offense = filter(lambda p: p.team == offense_team, players)
+    defense = filter(lambda p: p.team != offense_team, players)
+
+    # within each team, order by player position, break tie using player id
+    # {'C', 'F', 'F/C', 'G', 'G/F', 'PF', 'PG', 'SF', 'SG'}
+    offense = order_team_players(offense, gamecode)
+    defense = order_team_players(defense, gamecode)
+
+    players = []
+    players.extend(list(offense))
+    players.extend(list(defense))
+
+    return players
+                                                    
+def expand_trajectory(episode):
+    """
+    Fills in timeseries of x coordinates and y coordinates from beginning to end 
+    """
+    x_vals = {}
+    y_vals = {}
+    for player in range(11):
+        x_vals[p] = []
+        y_vals[p] = []
+    for frame in episode.frames:
+        x_vals[0].append(frame.ball.x)
+        y_vals[0].append(frame.ball.y)
+        for i, player in enumerate(order_players(frame, episode.offensive_team)):
+            x_vals[i+1].append(player.x)
+            y_vals[i+1].append(player.y)
+
+    final_ts = []
+
+    for player in range(11):
+        final_ts.append(np.array(x_vals[player]))
+        final_ts.append(np.array(y_vals[player]))
+
+    return np.array(final_ts)
+    
+def episode2ts(episode):
+    return torch.from_numpy(expand_trajectory(episode)).float()
+
+
+#################### main function #################################
+def transform_producer(up_to=1):
+    
+    def transform_ts_data(poss):
+        # chop it up, turn each episode into image data, and output (x, y)
+        # use information up to up_to second to build the image
+        episode, _ = cutOnce(poss, up_to)
+        x = episode2ts(episode)
+        y = poss.expected_outcome
+        return x, y
+    
+    return transform_ts_data
--- lib/model.py
+++ lib/model.py
@@ -26,5 +26,75 @@ class MLP(nn.Module):
         x = x.view(-1, self.neuron_sizes[0])
         return self.classifier(x)
 
+
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, dropout, use_gpu = False, output_dim = 1):
+        super(LSTM, self).__init__()
+        
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.output_dim = output_dim
+        self.lstm = nn.LSTM(input_size = int(input_size), hidden_size = int(hidden_size), num_layers = int(num_layers), dropout = float(dropout), batch_first = True)
+        self.use_gpu = use_gpu 
+
+        #Deal with Pytorch initialization for LSTMs being flawed
+        for name,param in self.lstm.named_parameters():
+            if 'weight' in name: 
+                init.xavier_uniform_(param)
+        
+        #TODO: Layer Norm?
+        
+        self.fc = nn.Linear(hidden_size, output_dim)
+        self.sm = nn.Sigmoid(dim=2)
+        self.float = self.FloatTensor
+        if use_gpu:
+            self.float = torch.cuda.FloatTensor
+            
+    def _get_final_y(self, output, batch_size, lengths):
+        '''Given output from linear layer, find the corresponding last timestep output'''
+        final_dist = []
+        for i in range(batch_size):
+            #Deals with uneven lengths in input
+            final_dist.append(output[i, lengths[i]-1].view(1, 1, self.output_dim))
+        return torch.cat(final_dist).view(batch_size, 1, self.output_dim)
+    
+    def forward(self, input, lengths):
+        input = input.numpy()
+        lengths = lengths.numpy() 
+        #Find lengths of all intervals
+        perm_index = reversed(sorted(range(len(lengths)), key=lambda k: lengths[k]))
+        
+        #Sort the input by length in order to pad, pack sequence for LSTM
+        input = [torch.tensor(input[i]) for i in perm_index]
+        lengths = list(reversed(sorted(lengths)))
+        padded = rnn.pad_sequence(input, batch_first = True).view(len(input), max(lengths), 1)
+        pack_padded = rnn.pack_padded_sequence(padded, lengths, batch_first = True)
+        
+        if self.use_gpu:
+            pack_padded = pack_padded.cuda()
+
+        #Get batch size, sequence length
+        batch_size = pack_padded.batch_sizes[0]
+        sequence_length = len(pack_padded.batch_sizes)
+        
+        #Initialize hidden state, cell state
+        h0 = Variable(torch.zeros(self.num_layers, batch_size, self.hidden_size).type(self.float), requires_grad = False)
+        c0 = Variable(torch.zeros(self.num_layers, batch_size, self.hidden_size).type(self.float), requires_grad = False)
+        
+                
+        lstm_output, h_and_c = self.lstm(pack_padded, (h0, c0))
+
+        #Unpack pad packed sequence, run through linear layer      
+        unpacked, lens = rnn.pad_packed_sequence(lstm_output,batch_first = True)
+        y = self.fc(unpacked)
+        
+        #Deal with uneven lengths
+        y_final = self.sm(self._get_final_y(y, batch_size, lengths))
+        
+        return y_final
+
+
 MODELS['MLP'] = MLP
+MODELS['LSTM'] = LSTM
 
